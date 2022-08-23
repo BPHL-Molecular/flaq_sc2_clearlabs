c1a3b9bcb0920f1a0ef0df861a102877c0a0e0ed
SESchmedes <sarahschmedes@gmail.com>
2022-08-23 10:48:56 -0400

    updated pangolin path and version in sbatch

--- sbatch_clearlabs_report.sh
+++ sbatch_clearlabs_report.sh
@@ -12,4 +12,4 @@
 #SBATCH --error=clearlabs_report.%j.err
 
 #Run script/command and use $SLURM_CPUS_ON_NODE
-python flaq_sc2_clearlabs.py --fastqs fastqs/ --assemblies assemblies/ --bams bams/ --threads $SLURM_CPUS_ON_NODE --sotc S:L452R,S:E484K --pango_path /apps/staphb-toolkit/containers/pangolin_4.1.2-pdata-1.12.sif --pangolin v4.1.2 --pangolin_data v1.12
+python flaq_sc2_clearlabs.py --fastqs fastqs/ --assemblies assemblies/ --bams bams/ --threads $SLURM_CPUS_ON_NODE --sotc S:L452R,S:E484K --pango_path /apps/staphb-toolkit/containers/pangolin_4.1.2-pdata-1.13.sif --pangolin v4.1.2 --pangolin_data v1.13
